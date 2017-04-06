be583c3a29c69b7cb49ef8e0e15f79957402c437
Bülent Sezer <bulent.sezer@everymatrix.com>
2017-04-06 15:52:40 +0300

    Delete unnecessary comments

--- Assets/floppy/ui.js
+++ Assets/floppy/ui.js
@@ -10,21 +10,6 @@ function Start () {
 
 function Update () {
     scoreBoard.text="Score : " + bird.score;
-//	if (bird.score == 1) {
-//		scor.text="Surprisingly, you made it here";
-//	}
-//	else if (bird.score == 2) {
-//		scor.text="Wow, how did you go over 1?";
-//	}
-//	else if (bird.score == 3) {
-//		scor.text="I will asume you just have luck";
-//	}
-//	else if (bird.score == 4) {
-//		scor.text="This surely isn't you playing";
-//	}
-//	else if (bird.score == 5) {
-//		scor.text="Prepare for the challenges";
-//	}
 	var sez = ["","Surprisingly, you made it here", "Wow, how did you go over 1?", "Your cat would've finished already"];
 	var sezLength = sez.length;
 	for (var i = 0; i <= bird.score; i++) {
